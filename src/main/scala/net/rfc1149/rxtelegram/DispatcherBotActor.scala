87ea2f8ffef61971759f02cbe864e8585f8da633
Samuel Tardieu <sam@rfc1149.net>
2016-11-14 23:37:14 +0100

    Do not block actor restart on message wait

--- src/main/scala/net/rfc1149/rxtelegram/DispatcherBotActor.scala
+++ src/main/scala/net/rfc1149/rxtelegram/DispatcherBotActor.scala
@@ -9,17 +9,12 @@ abstract class DispatcherBotActor(token: String, options: Options) extends BotAc
 
   def createActor(chat: Chat, message: Message, context: ActorContext): Option[ActorRef]
 
-  private[this] def registerChatActor(chat_id: Long, actorRef: ActorRef): Unit = {
-    actorRef ! (me, chat_id)
-    addChat(chat_id, actorRef)
-  }
-
   override def handleMessage(message: Message): Unit = {
     val chat = message.chat
     val chat_id = chat.id
     actorRef(chat_id).fold {
       for (actorRef ← createActor(chat, message, context)) {
-        registerChatActor(chat_id, actorRef)
+        addChat(chat_id, actorRef)
         actorRef ! message
       }
     } {
@@ -29,7 +24,7 @@ abstract class DispatcherBotActor(token: String, options: Options) extends BotAc
 
   override def handleOther(other: Any): Unit = other match {
     case CreateChat(chat_id: Long, props: Props, name: String) ⇒
-      registerChatActor(chat_id, context.actorOf(props, name))
+      addChat(chat_id, context.actorOf(props, name))
     case RemoveChat(id) ⇒
       removeChat(id)
     case _ ⇒
